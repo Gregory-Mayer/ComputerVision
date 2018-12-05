61b868df10b8eedb2e498ad27d6534f377a93823
L-Albano <32752628+L-Albano@users.noreply.github.com>
2018-12-05 14:03:44 -0500

    update of code and 7000-3000 cascade

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -8,189 +8,132 @@
 #include <iomanip>
 using namespace cv;
 using namespace std;
-//class Detector
-//{
-//	enum Mode { Default, Daimler } m;
-//	HOGDescriptor hog, hog_d;
-//public:
-//	Detector() : m(Default), hog(), hog_d(Size(48, 96), Size(16, 16), Size(8, 8), Size(8, 8), 9)
-//	{
-//		hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
-//		hog_d.setSVMDetector(HOGDescriptor::getDaimlerPeopleDetector());
-//	}
-//	void toggleMode() { m = (m == Default ? Daimler : Default); }
-//	string modeName() const { return (m == Default ? "Default" : "Daimler"); }
-//	vector<Rect> detect(InputArray img)
-//	{
-//		// Run the detector with default parameters. to get a higher hit-rate
-//		// (and more false alarms, respectively), decrease the hitThreshold and
-//		// groupThreshold (set groupThreshold to 0 to turn off the grouping completely).
-//		vector<Rect> found;
-//		if (m == Default)
-//			hog.detectMultiScale(img, found, 0, Size(8, 8), Size(32, 32), 1.05, 2, false);
-//		else if (m == Daimler)
-//			hog_d.detectMultiScale(img, found, 0.5, Size(8, 8), Size(32, 32), 1.05, 2, true);
-//		return found;
-//	}
-//	void adjustRect(Rect & r) const
-//	{
-//		// The HOG detector returns slightly larger rectangles than the real objects,
-//		// so we slightly shrink the rectangles to get a nicer output.
-//		r.x += cvRound(r.width*0.1);
-//		r.width = cvRound(r.width*0.8);
-//		r.y += cvRound(r.height*0.07);
-//		r.height = cvRound(r.height*0.8);
-//	}
-//};
+
+int boundLower(int var);
+int boundUpper(int var);
 
 int main(int argc, char** argv)
 {
-	// capture from web camera init
-
-	//VideoCapture cap(0);
-	//cap.open(0);
+	//argv[0] its the .exe
+	//argv[1] will the image file
+	//argv[2] for setting no fly zone 
+	//argv[3-6]
+	int start_x = 0;
+	int start_y = 0;
+	int end_x = 0;
+	int end_y = 0;
+	String fileName;
+	/*
+	if (argc < 2) {
+		cout << "missing image file" << endl;
+		return 0;
+	}
+	else if (argc == 2) {
+		fileName = argv[1];
+		cout << fileName << endl;
+	}
+	else if (argc < 6 || argc >= 6) {
+		fileName = argv[1];
+		start_x = stoi(argv[2]);
+		start_y = stoi(argv[3]);
+		end_x = stoi(argv[4]);
+		end_y = stoi(argv[5]);
+	}
+	else {
+		cout << "invalid arguments" << endl;
+		return 0;
+	}
+	*/
 
 	Mat img;
 
-	// Initialize the inbuilt Harr Cascade frontal face detection
-	// Below mention the path of where your haarcascade_frontalface_alt2.xml file is located
-
-	//CascadeClassifier face_cascade;
-	//face_cascade.load("C:\\opencv\\sources\\data\\haarcascades\\haarcascade_frontalface_alt2.xml");
-
-	//Detector detector;
-	//Mat frame;
-	//for (;;)
-	//{
-	//	cap >> frame;
-	//	if (frame.empty())
-	//	{
-	//		cout << "Finished reading: empty frame" << endl;
-	//		break;
-	//	}
-	//	int64 t = getTickCount();
-	//	vector<Rect> found = detector.detect(frame);
-	//	t = getTickCount() - t;
-
-	//	// show the window
-	//	{
-	//		ostringstream buf;
-	//		buf << "Mode: " << detector.modeName() << " ||| "
-	//			<< "FPS: " << fixed << setprecision(1) << (getTickFrequency() / (double)t);
-	//		putText(frame, buf.str(), Point(10, 30), FONT_HERSHEY_PLAIN, 2.0, Scalar(0, 0, 255), 2, LINE_AA);
-	//	}
-	//	for (vector<Rect>::iterator i = found.begin(); i != found.end(); ++i)
-	//	{
-	//		Rect &r = *i;
-	//		detector.adjustRect(r);
-	//		rectangle(frame, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
-	//	}
-	//	imshow("People detector", frame);
-
-	//	// interact with user
-	//	const char key = (char)waitKey(30);
-	//	if (key == 27 || key == 'q') // ESC
-	//	{
-	//		cout << "Exit requested" << endl;
-	//		break;
-	//	}
-	//	else if (key == ' ')
-	//	{
-	//		detector.toggleMode();
-	//	}
-	//}
-
-	
-
-
-	//Camera face detection
-	/*
-	for (;;)
-	{
-
-		// Image from camera to Mat
-
-		cap >> img;
-
-		// obtain input image from source
-		cap.retrieve(img, CV_CAP_OPENNI_BGR_IMAGE);
-
-		// Just resize input image if you want
-		resize(img, img, Size(1000, 640));
-
-		// Container of faces
-		vector<Rect> faces;
-
-
-		// Detect faces
-		face_cascade.detectMultiScale(img, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(140, 140));
-
-
-		//Show the results
-		// Draw circles on the detected faces
-
-		for (int i = 0; i < faces.size(); i++)
-		{
-			Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
-			ellipse(img, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-		}
-
-		// To draw rectangles around detected faces
-	    //for (unsigned i = 0; i<faces.size(); i++)
-				   //rectangle(img,faces[i], Scalar(255, 0, 0), 2, 1);
-
-
-		imshow("Computer Vision", img);
-		int key2 = waitKey(20);
-
-	}*/
-
-
-
-	/*
-	vector<String> fn; 
-	string path = "C:/Users/Serenity/Desktop/UMBC HW/AI/planesnet/*.png";
-	glob(path, fn, false);
-	
-	int i = 0;
-	img = imread(fn.front(), CV_LOAD_IMAGE_GRAYSCALE);
-	namedWindow("testies", WINDOW_NORMAL);
-	resizeWindow("testies", 500,500);
-	for (;;)
-	{
-		
-		cout << i << "\n" << fn[i] << "\n";
-		imshow("testies", img);
-		int key2 = waitKey(20);
-		i++;
-		if (i>fn.size()-1) 
-		{
-			i = fn.size()-1;
-		}
-		img = imread(fn[i], CV_LOAD_IMAGE_GRAYSCALE);
-		
-	}*/
-
 	CascadeClassifier plane_classifier;
-	plane_classifier.load("/ComputerVision/landons junk folder/cascade/cascade.xml");
-	img = imread("/ComputerVision/landons junk folder/test6.jpg", CV_LOAD_IMAGE_ANYDEPTH);
+	//cout << "test1" << endl; 
+
+	
+	plane_classifier.load("C:/Users/Serenity/Desktop/UMBC HW/AI/final_cascade/cascade.xml");
+	//plane_classifier.load("C:/Users/Serenity/Desktop/UMBC HW/AI/_newTraining/cascade30/cascade.xml");
+	//cout << "test1" << endl;
+	
+	//C:\Users\Serenity\source\repos\ComputerVision\landons junk folder\test6.jpg
+	fileName = "C:/Users/Serenity/source/repos/ComputerVision/landons junk folder/test6.jpg";
+	img = imread(fileName, CV_LOAD_IMAGE_COLOR);
+
+	
+	int coords[4] = {start_x,start_y,end_x,end_y};
+	//int coords[4] = {50,300,550,250};
+	//unneeded input checking
+	/*
+	int width = img.col;
+	int height = img.row;
+	for (int i = 0; i < 4; i++) {
+		coords[i] = boundLower(coords[i]);
+		if (i%2 == 0) {
+			coords[i] = boundUpper(coords[i], width);
+		}
+		else {
+			coords[i] = boundUpper(coords[i], height);
+		}
+		
+	}*/
+
+	Rect noFly(coords[0], coords[1], coords[2], coords[3]);
+	//cout << "test1" << endl;
 	//for (;;)
 	//{
 		
 		vector<Rect> planes;
-		plane_classifier.detectMultiScale(img, planes, 1.1, 3, 0 | CV_HAAR_SCALE_IMAGE, Size(1, 1));
+		//plane_classifier.detectMultiScale(img, planes, 1.1, 3, 0 | CV_HAAR_SCALE_IMAGE, Size(1, 1));
+		//plane_classifier.detectMultiScale(img, planes, 1.1, 3, 0, Size(1,1), Size(50,50));
+		plane_classifier.detectMultiScale(img, planes, 1.1, 8, 0, Size(0.5, 0.5), Size(25, 25));
+		//cout << "test1" << endl;
 		for (int i = 0; i < planes.size(); i++)
 		{
+			//cout << "test21" << endl;
 			Point center(planes[i].x + planes[i].width*0.5, planes[i].y + planes[i].height*0.5);
-			ellipse(img, center, Size(planes[i].width*0.5, planes[i].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+			if (noFly.contains(center)) {
+				ellipse(img, center, Size(planes[i].width*0.5, planes[i].height*0.5), 0, 0, 360, Scalar(0, 255,255), 2, 8, 0);
+			}
+			else {
+				ellipse(img, center, Size(planes[i].width*0.5, planes[i].height*0.5), 0, 0, 360, Scalar(255, 255, 0), 2, 8, 0);
+			}
+			//ellipse(img, center, Size(planes[i].width*0.5, planes[i].height*0.5), 0, 0, 360, Scalar(0, 255, 0), 2, 8, 0);
 		}
-
+		Point org;
+		org.x = 0;
+		org.y = img.size().height - 10;
+		RNG rng(0xFFFFFFFF);
+		putText(img, "Planes: " + to_string(planes.size()), org, FONT_HERSHEY_SIMPLEX, 2, Scalar(0,0,0), 5, 8);
+		putText(img, "Planes: " + to_string(planes.size()), org, FONT_HERSHEY_SIMPLEX, 2, Scalar(255, 255, 255), 3, 8);
+		//Rect noFly();
+		//rectangle(img, Point(50, 50), Point(100, 100), Scalar(0,0,255), 5);
+		rectangle(img, noFly, Scalar(0, 0, 255), 5);
 		imshow("test", img);
-		int key3 = waitKey(30);
+		imwrite("C:/Users/Serenity/source/repos/ComputerVision/landons junk folder/results/Results.jpg", img);
+		int key3 = waitKey(0);
 	//}
-	imwrite("/ComputerVision/results/Results.jpg", img);
+		//cout << "test1" << endl;
+
+	
 	
 	return 0;
 }
 
 
+int boundLower(int var) {
+	if (var < 0) {
+		return 0;
+	}
+	else {
+		return var;
+	}
+}
+
+int boundUpper(int var, int max) {
+	if (var > max) {
+		return max;
+	}
+	else {
+		return var;
+	}
+}
